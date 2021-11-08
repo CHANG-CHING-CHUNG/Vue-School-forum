138e90484030743b6eef7f77f0ceec97771eeada
John <john.chang@nexus-techltd.com>
2021-11-08 20:05:55 +0800

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,30 @@
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-import PageNotFound from '@/components/PageNotFound'
+import Home from '@/pages/Home'
+import ThreadShow from '@/pages/ThreadShow'
+import NotFound from '@/pages/NotFound'
 import { createRouter, createWebHistory } from 'vue-router'
 import sourceData from '@/data.json'
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: PageHome
+    component: Home
   },
   {
     path: '/thread/:id',
     name: 'ThreadShow',
-    component: PageThreadShow,
+    component: ThreadShow,
     props: true,
     beforeEnter (to, from, next) {
       const threadExists = sourceData.threads.find(thread => thread.id === to.params.id)
       if (threadExists) {
         return next()
       } else {
-        next({ name: 'PageNotFound', params: { pathMatch: to.path.substring(1).split('/') }, query: to.query, hash: to.hash })
+        next({ name: 'NotFound', params: { pathMatch: to.path.substring(1).split('/') }, query: to.query, hash: to.hash })
       }
     }
   },
   {
-    path: '/:pathMatch(.*)*', name: 'PageNotFound', component: PageNotFound
+    path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound
   }
 ]
 
